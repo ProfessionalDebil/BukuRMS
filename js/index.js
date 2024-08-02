1eb3d15316db965cdba74ddb59cbcca3d7989b9a
Debil <fransfelix100@gmail.com>
2024-08-02 22:52:33 +0700

    Upload basic functionality

---
+++ js/index.js
@@ -0,0 +1,35 @@
+var page = 0;
+var leftPage = document.getElementById("leftPage");
+var rightPage = document.getElementById("rightPage");
+
+function left() {
+    page = Math.max(page - 1, 0)
+    console.log(page);
+
+    leftPage.src = "pages/" + (2 * page).toString() + ".png";
+    rightPage.src = "pages/" + (2 * page + 1).toString() + ".png";
+}
+
+function right() {
+    page = Math.min(page + 1, 6)
+    console.log(page);
+
+    leftPage.src = "pages/" + (2 * page).toString() + ".png";
+    rightPage.src = "pages/" + (2 * page + 1).toString() + ".png";
+}
+
+function min(a, b) {
+    if (a > b) {
+        return b;
+    }
+
+    return a;
+}
+
+function max(a, b) {
+    if (a < b) {
+        return b;
+    }
+
+    return a;
+}
