5c8df80fbda31f76f2c57fb7662a63d080bc521d
Debil <fransfelix100@gmail.com>
2024-08-14 13:03:59 +0700

    add file type option

--- js/index.js
+++ js/index.js
@@ -2,20 +2,33 @@ var page = 0;
 var leftPage = document.getElementById("leftPage");
 var rightPage = document.getElementById("rightPage");
 
+const params = new Proxy(new URLSearchParams(window.location.search), {
+  get: (searchParams, prop) => searchParams.get(prop),
+});
+
+var type = ".jpg"
+
+if (params.png == "true") {
+    type = ".png"
+
+    leftPage.src = "pages/0.png";
+    rightPage.src = "pages/1.png";
+}
+
 function left() {
     page = Math.max(page - 1, 0)
     console.log(page);
 
-    leftPage.src = "pages/" + (2 * page).toString() + ".png";
-    rightPage.src = "pages/" + (2 * page + 1).toString() + ".png";
+    leftPage.src = "pages/" + (2 * page).toString() + type;
+    rightPage.src = "pages/" + (2 * page + 1).toString() + type;
 }
 
 function right() {
     page = Math.min(page + 1, 7)
     console.log(page);
 
-    leftPage.src = "pages/" + (2 * page).toString() + ".png";
-    rightPage.src = "pages/" + (2 * page + 1).toString() + ".png";
+    leftPage.src = "pages/" + (2 * page).toString() + type;
+    rightPage.src = "pages/" + (2 * page + 1).toString() + type;
 }
 
 function min(a, b) {
